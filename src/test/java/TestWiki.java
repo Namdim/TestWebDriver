e3b05fcd5e62a52e7e19694d8ce149e38aa76d56
Anatolii Nizkopoklonnyi <a.nizkopoklonnyi@gmail.com>
2018-09-20 11:41:14 +0300

    Go to the Wiki home page

--- src/test/java/TestWiki.java
+++ src/test/java/TestWiki.java
@@ -1,9 +1,13 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,10 +26,18 @@ public class TestWiki {
 
     @BeforeClass // Runs this method before the first test method in the current class is invoked
     public void setUp() {
+        driver = new ChromeDriver(service);
     }
 
-    @Test // Run web driver(Chrome)
-    public void checkWebDriver(){
-        driver = new ChromeDriver(service);
+    @Test
+    public void gotoWikiPage() {
+        // Go to the Wikipedia home page
+        driver.get("https://en.wikipedia.org/");
+
+        }
+
+    @AfterClass
+    public void closeConnection(){
+        driver.close();
     }
 }
